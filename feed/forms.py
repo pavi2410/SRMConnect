67de944899905b573a39722f67b40852c7e07769
Pavitra Golchha <pavitragolchha@gmail.com>
2021-04-11 21:10:57 +0530

    posts

--- feed/forms.py
+++ feed/forms.py
@@ -5,7 +5,7 @@ from .models import Comments, Post
 class NewPostForm(forms.ModelForm):
 	class Meta:
 		model = Post
-		fields = ['description', 'pic', 'tags']
+		fields = ['description', 'tags']
 
 
 class NewCommentForm(forms.ModelForm):
--- feed/models.py
+++ feed/models.py
@@ -8,7 +8,6 @@ from django.utils import timezone
 
 class Post(models.Model):
 	description = models.CharField(max_length=255, blank=True)
-	pic = models.ImageField(upload_to='path/to/img')
 	date_posted = models.DateTimeField(default=timezone.now)
 	user_name = models.ForeignKey(User, on_delete=models.CASCADE)
 	tags = models.CharField(max_length=100, blank=True)

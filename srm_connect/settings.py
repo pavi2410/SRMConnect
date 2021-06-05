018a207d7a75a2969b60db4a4a7bd2cd372acc80
Pavitra Golchha <pg7517@srmist.edu.in>
2021-06-05 21:50:00 +0530

    Update settings.py

--- srm_connect/settings.py
+++ srm_connect/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,5 @@ MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())

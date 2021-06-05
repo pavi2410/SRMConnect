f669dea4a819426034c5fc76ced97337408846c2
Pavitra Golchha <pg7517@srmist.edu.in>
2021-06-05 21:27:14 +0530

    Update settings.py

--- srm_connect/settings.py
+++ srm_connect/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=13+3z9cu#xzbp=2$f=zw$0v+by3qk(wry)42of4z+!@+gee@n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 

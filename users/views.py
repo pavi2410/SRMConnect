d273e3c15e2e8c273c17fe10145951fbcac1980f
Pavitra Golchha <pavitragolchha@gmail.com>
2021-06-08 00:30:44 +0530

    Final changes

--- users/views.py
+++ users/views.py
@@ -52,9 +52,32 @@ def users_list(request):
 
 def friend_list(request):
 	p = request.user.profile
+	u = p.user
 	friends = p.friends.all()
+	sent_friend_requests = FriendRequest.objects.filter(from_user=p.user)
+	rec_friend_requests = FriendRequest.objects.filter(to_user=p.user)
+	
+	# is this user our friend
+	button_status = 'none'
+	if p not in request.user.profile.friends.all():
+		button_status = 'not_friend'
+
+		# if we have sent him a friend request
+		if len(FriendRequest.objects.filter(
+			from_user=request.user).filter(to_user=p.user)) == 1:
+			button_status = 'friend_request_sent'
+
+		# if we have recieved a friend request
+		if len(FriendRequest.objects.filter(
+			from_user=p.user).filter(to_user=request.user)) == 1:
+			button_status = 'friend_request_received'
+
 	context = {
-            'friends': friends
+		'u': u,
+		'friends': friends,
+		'button_status': button_status,
+		'sent_friend_requests': sent_friend_requests,
+		'rec_friend_requests': rec_friend_requests,
 	}
 	return render(request, "users/friend_list.html", context)
 
